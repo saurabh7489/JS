64623193075a7718f91fd79c290fe9cc27493986
saurabh7489 <saurabhpandey7489@gmail.com>
2026-08-29 22:47:41 +0530

    initial commit

---
+++ variable.js
@@ -0,0 +1,15 @@
+const userId=456231; //we can not update or modify const veriable
+
+let userEmail="saurabh@gmail.com"; //we can update it 
+
+var userPassward="jaihooo"; // udate it but prefer not to use it 
+
+userlocation="jabalpur"; // update but this is not a good why to write a code
+
+let userState;
+// userId=54782;
+userEmail="sourabh@gmail.com";
+userPassward="jaijaijaihooo";
+userlocation="mumbai";
+
+console.table([userId,userEmail,userPassward,userlocation,userState]);
