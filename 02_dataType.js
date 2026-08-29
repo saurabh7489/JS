77ab796859a76b258e079148fb6e336a24d2899e
saurabh7489 <saurabhpandey7489@gmail.com>
2026-08-29 23:17:12 +0530

    initial commit

---
+++ 02_dataType.js
@@ -0,0 +1,9 @@
+// there are various type of data type such as
+// integer,string,boolean,big int,undefined,and object=null;
+let name="sourabh" //string
+let age=20 //integer or number
+let isLoggedIn =true //boolean
+let temp=null; // object 
+let addharCard; //undefined
+
+console.table([  typeof name,typeof age,typeof isLoggedIn,typeof temp,typeof addharCard])
