bc015243fc8dd98ec2556fc62976d98c9f89651a
saurabh7489 <saurabhpandey7489@gmail.com>
2026-08-30 17:43:26 +0530

    initial commit

--- class/03_conversionOperation.js
+++ class/03_conversionOperation.js
@@ -14,3 +14,27 @@ let id=4568
 // console.log(typeof name)
 // console.log(typeof convNumber)
 // console.log( convNumber) //nan = non a number
+
+// ******************* Operators ****************************
+
+// ++ => Opertators 
+// x ,a , b => Operants
+
+//  console.log(2+3)
+//  console.log(2-3)
+//  console.log(2*2)
+//  console.log(2**3)
+//  console.log(2/3)
+//  console.log(2%3)
+
+// let name ="Saurabh "
+// let lastName ="Pandey" 
+// fullName=name+lastName
+// console.log("full name of user := "+fullName);
+
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+// console.log("1"+2)
+// console.log(1+"2")
+
+// this is the basic operators in javaScript
