37ade86292a4c861a7d6c11622ec9f6325c049d9
saurabh7489 <saurabhpandey7489@gmail.com>
2026-08-30 16:49:36 +0530

    initial commit

---
+++ class/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let id=4568
+
+// console.log(typeof id)
+// let convString=String(id)
+// console.log(typeof convString)
+
+// let isLoggin = 0;
+// let convBoolean= Boolean(isLoggin)
+// console.log(typeof isLoggin)
+// console.log( convBoolean)
+
+// let name="Saurabh"
+// let convNumber=Number(name)
+// console.log(typeof name)
+// console.log(typeof convNumber)
+// console.log( convNumber) //nan = non a number
