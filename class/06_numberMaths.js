534584c8ea250b52a600b6c6587a70d9a1202312
saurabh7489 <saurabhpandey7489@gmail.com>
2026-09-01 01:24:25 +0530

    initial commit

---
+++ class/06_numberMaths.js
@@ -0,0 +1,38 @@
+// let score = 100
+// console.log(score);
+
+// const number = new Number(10000000)
+// console.log(number);
+// console.log(number.toString().length);
+// console.log(number.toFixed(2))
+// console.log(number.toPrecision(3));
+// console.log(number.toLocaleString("en-In"));
+
+// *********************Maths***********************
+
+// console.log(Math);
+// console.log(Math.round(4.6));
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(49));
+// console.log(Math.min(3,4,5,6,7));
+// console.log(Math.max(56,7,8,9));
+// console.log(Math.random(1,6));
+// console.log((Math.random()*10)+1);
+let min =5;
+let max = 10;
+console.log(Math.random());
+console.log(max-min+1);
+
+console.log(Math.floor(Math.random()))
+console.log(Math.random()*(max-min+1))
+console.log(Math.floor(Math.random()*(max-min+1)));
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
