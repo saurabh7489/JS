03c3b6995176f26eab170792354171ea6b099170
saurabh7489 <saurabhpandey7489@gmail.com>
2026-09-01 01:27:00 +0530

    initial commit

--- class/06_numberMaths.js
+++ class/06_numberMaths.js
@@ -22,12 +22,12 @@
 // console.log((Math.random()*10)+1);
 let min =5;
 let max = 10;
-console.log(Math.random());
-console.log(max-min+1);
+// console.log(Math.random());
+// console.log(max-min+1);
 
-console.log(Math.floor(Math.random()))
-console.log(Math.random()*(max-min+1))
-console.log(Math.floor(Math.random()*(max-min+1)));
+// console.log(Math.floor(Math.random()))
+// console.log(Math.random()*(max-min+1))
+// console.log(Math.floor(Math.random()*(max-min+1)));
 console.log(Math.floor(Math.random()*(max-min+1))+min);
 
 
