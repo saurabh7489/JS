0ddddd9214bde12ca97a74bada61a0966ff692b7
saurabh7489 <saurabhpandey7489@gmail.com>
2026-09-03 21:36:25 +0530

    initial commit

---
+++ 2class/01_Array.js
@@ -0,0 +1,26 @@
+const myarray = [1,2,3,true,"saurabh"]
+// console.log(typeof myarray);
+// const newarray = new Array(1,2,3,7,"pandey")
+// console.log(typeof newarray);
+
+//  myarray.push(2)
+// console.log(myarray);
+// myarray.pop()
+// console.log(myarray);
+
+// myarray.join()
+// console.log(myarray.join());
+
+// slice and splice 
+
+console.log(myarray);
+
+console.log(myarray.slice(1,3));
+console.log(myarray);
+
+
+console.log(myarray.splice(1,3));
+console.log(myarray);
+
+
+
