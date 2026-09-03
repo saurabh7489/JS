19d1a59579b8e70f5a870cc8628a22ecbb61702c
saurabh7489 <saurabhpandey7489@gmail.com>
2026-09-03 23:22:46 +0530

    initial commit

--- 2class/01_Array.js
+++ 2class/01_Array.js
@@ -1,4 +1,4 @@
-const myarray = [1,2,3,true,"saurabh"]
+// const myarray = [1,2,3,true,"saurabh"]
 // console.log(typeof myarray);
 // const newarray = new Array(1,2,3,7,"pandey")
 // console.log(typeof newarray);
@@ -13,14 +13,39 @@ const myarray = [1,2,3,true,"saurabh"]
 
 // slice and splice 
 
-console.log(myarray);
+// console.log(myarray);
 
-console.log(myarray.slice(1,3));
-console.log(myarray);
+// console.log(myarray.slice(1,3));
+// console.log(myarray);
 
 
-console.log(myarray.splice(1,3));
-console.log(myarray);
+// console.log(myarray.splice(1,3));
+// console.log(myarray);
 
+const marvel=["ironmen","caption","hulk"]
+const dc =["batman","flash","superman"]
+
+// marvel.push(dc)
+// console.log(marvel[3][2]);
+// marvel.concat(dc)
+// console.log(marvel);
+
+// console.log(marvel.concat(dc));
+
+// const merge=[...marvel,...dc]
+// console.log(merge);
+
+// const newarray = [1,2,3,[4,5,6],7,8,[2,3,4,[1,2,3]],7]
+// console.log(newarray.flat(Infinity));
+
+// console.log(Array.from("Saurabh"));
+
+// let score=200;
+// let score2 = 300;
+// let score3 = 400;
+
+// console.log(Array.of(score,score2,score3));
+
+// console.log(Array.from({name:"saurabh"})); //intresting 
 
 
