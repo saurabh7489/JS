67a1d623c869e8ed8795eaaf9dcef7a22ff7ff43
saurabh7489 <saurabhpandey7489@gmail.com>
2026-09-06 00:47:18 +0530

    initial commit

---
+++ 2class/02_object.js
@@ -0,0 +1,83 @@
+// the keys is always string
+const mySymbol = Symbol("key006")
+const user={
+    "name":"saurabh",
+    class:"inCollege",
+     [mySymbol]:"key1",
+    isLogin:true,
+    "age":20,
+    email:"Saurabh@jaihoo.com",
+    hobby:["reading ","writingJurnals",]
+}
+// // console.log(  user[mySymbol]);
+// // user.email="saurabh@sherrr.com"
+// // // Object.freeze(user)
+// // user.email="saurabh@depresion.com"
+// // console.log(user);
+
+// user.intro = function(){
+//     console.log("hello i am saurabh");
+    
+// }
+
+// console.log(user.intro());
+
+// user.greeting=function(){
+//     console.log(`hello this is for adding somthing from object like name ex: ${this.name}`);
+    
+// }
+// console.log(user.greeting());
+
+// const user = new Object()
+// console.log(user);
+
+// const user={}
+// user.id="123jaihoo"
+// user.name="shahoooo"
+// console.log(user);
+
+// const user={
+//     name:{
+// fullname:{
+//     firstname:"saurabh",
+//     lastname:"pandey",
+//     username:{
+//         platforusername:"saurabh7489"
+//     }
+// }
+
+//     },
+//     age:20,
+// }
+// console.log(user.name.fullname.username.platforusername);
+
+
+// const obj={1:"a",2:"b",3:"c"};
+// const obj2={4:"d",5:"e",6:"f"};
+// const obj3=({},obj,obj2)
+// const obj3=[...obj,...obj2]
+// const obj3={obj,obj2}
+// const obj3=Object.assign({},obj,obj2)
+
+// const obj3={...obj,...obj2}
+
+
+// console.log(obj3);
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+
+// console.log(Object.entries(user));
+
+
+// console.log(user.hasOwnProperty('email2'));
+// console.log(user.email);
+// console.log(user.email);
+// console.log(user.email);
+// console.log(email);
+// const {name:jaihoo}=user;
+// console.log(email);
+// console.log(user.email);
+// console.log(jaihoo);
+
+
